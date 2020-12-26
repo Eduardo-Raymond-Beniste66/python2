2e9e9712219aedda2a9c7ea6c78c7179cac0083a
Eduardo Raymond Beniste <76535927+Eduardo-Raymond-Beniste66@users.noreply.github.com>
2020-12-26 19:34:19 -0300

    ExercicioDeSoma deu erro
    Eduardo Raymond Beniste

---
+++ ExercicioDeSoma.py
@@ -0,0 +1,10 @@
+soma = 0
+n = 0
+while true:
+    x = int(input('n(Zero: Sai:'))
+    if x == 0:
+        break
+    else:
+        n = n +  1
+        soma = soma + 1
+        print(f'Media: {soma/n:.1f}')
